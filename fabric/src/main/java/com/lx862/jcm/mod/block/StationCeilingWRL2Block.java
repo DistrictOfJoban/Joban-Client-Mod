c324c4218698361c0cfcb0d3d097abc299f2fc42
LX862 <kenny.mh.hui@outlook.com>
2023-12-02 17:41:29 +0800

    Fix Station Ceiling not rotating on place

--- fabric/src/main/java/com/lx862/jcm/mod/block/StationCeilingWRL2Block.java
+++ fabric/src/main/java/com/lx862/jcm/mod/block/StationCeilingWRL2Block.java
@@ -15,13 +15,13 @@ public class StationCeilingWRL2Block extends VerticallyAttachedDirectional2Block
     public VoxelShape getOutlineShape2(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         switch (BlockUtil.getProperty(state, PART)) {
             case 0:
-                VoxelShape vx1 = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 0.5, 8, 1, 16, 9, 15);
-                VoxelShape vx2 = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 5.5, 9, 7.5, 6.5, 16, 8.5);
-                return VoxelShapes.union(vx1, vx2);
+                VoxelShape ceiling = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 0.5, 8, 1, 16, 9, 15);
+                VoxelShape pole = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 5.5, 9, 7.5, 6.5, 16, 8.5);
+                return VoxelShapes.union(ceiling, pole);
             default:
-                VoxelShape vx1R = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 0, 8, 1, 15.5, 9, 15);
-                VoxelShape vx2R = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 10.5, 9, 7.5, 11.5, 16, 8.5);
-                return VoxelShapes.union(vx1R, vx2R);
+                VoxelShape ceilingR = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 0, 8, 1, 15.5, 9, 15);
+                VoxelShape poleR = VoxelUtil.getDirectionalShape16(BlockUtil.getProperty(state, FACING), 10.5, 9, 7.5, 11.5, 16, 8.5);
+                return VoxelShapes.union(ceilingR, poleR);
         }
     }
 
--- fabric/src/main/java/com/lx862/jcm/mod/block/base/VerticallyAttachedDirectionalBlock.java
+++ fabric/src/main/java/com/lx862/jcm/mod/block/base/VerticallyAttachedDirectionalBlock.java
@@ -15,6 +15,11 @@ public abstract class VerticallyAttachedDirectionalBlock extends VerticallyAttac
     }
 
     @Override
+    public BlockState getPlacementState2(ItemPlacementContext ctx) {
+        return super.getPlacementState2(ctx) == null ? null : super.getPlacementState2(ctx).with(new Property<>(FACING.data), ctx.getPlayerFacing().data);
+    }
+
+    @Override
     public void addBlockProperties(List<HolderBase<?>> properties) {
         super.addBlockProperties(properties);
         properties.add(FACING);
