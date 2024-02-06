5355a7b44322503be8753d3bc26713d4ddcf21a7
LX862 <kenny.mh.hui@outlook.com>
2024-02-06 19:55:36 +0800

    Make Fare Saver save balance

--- fabric/src/main/java/com/lx862/jcm/mod/util/TextCategory.java
+++ fabric/src/main/java/com/lx862/jcm/mod/util/TextCategory.java
@@ -1,9 +1,14 @@
 package com.lx862.jcm.mod.util;
 
 public enum TextCategory {
-    HUD("hud"),
+    /** Block name */
     BLOCK("block"),
+    /** Item name */
     ITEM("item"),
+    /** In-game overlay, such as the action bar text */
+    HUD("hud"),
+
+    /** GUI Interfaces, usually element that are displayed in a screen user can interact with. */
     GUI("gui");
 
     final String prefix;
