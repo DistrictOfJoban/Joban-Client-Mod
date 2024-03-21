2fbd6ed500c3f1b5faa1ab8bfef8fc93eab4a57e
LX862 <kenny.mh.hui@outlook.com>
2024-03-22 00:29:57 +0800

    Remove not well-adapted mixin
    Why forge why

--- forge/src/main/java/com/lx862/jcm/mixin/compat/SimpleDefaultedRegistryMixin.java
+++ forge/src/main/java/com/lx862/jcm/mixin/compat/SimpleDefaultedRegistryMixin.java
@@ -1,5 +1,5 @@
 package com.lx862.jcm.mixin.compat;
-
+/*
 import com.lx862.jcm.mod.util.JCMUtil;
 #if MC_VERSION < "11904"
 import net.minecraft.core.DefaultedRegistry;
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
+*/
 /**
  * Block renamed in JCM 2.0.0, unfortunately there's no easy way to migrate ids.
  * This class helps migrate these ids
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
  */
 
 // TODO: Mixin injection failure
-
+/*
 #if MC_VERSION < "11904"
     @Mixin(DefaultedRegistry.class)
 #else
@@ -32,4 +32,4 @@ public class SimpleDefaultedRegistryMixin {
         if(id == null) return null;
         return JCMUtil.getMigrationId(new org.mtr.mapping.holder.Identifier(id)).data;
     }
-}
+}*/
