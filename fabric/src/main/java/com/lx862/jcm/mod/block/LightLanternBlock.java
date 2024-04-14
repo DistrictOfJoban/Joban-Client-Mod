b8bc33c7d30c5e0c26d14fee17bc2adbdd406d5e
dooji2 <cmir79411@gmail.com>
2024-04-14 21:43:29 +0300

    Add spotlight functionality to light lantern block

--- fabric/src/main/java/com/lx862/jcm/mod/block/LightLanternBlock.java
+++ fabric/src/main/java/com/lx862/jcm/mod/block/LightLanternBlock.java
@@ -1,10 +1,18 @@
 package com.lx862.jcm.mod.block;
 
 import com.lx862.jcm.mod.block.base.DirectionalBlock;
+import com.lx862.jcm.mod.data.BlockProperties;
 import com.lx862.jcm.mod.util.VoxelUtil;
 import org.mtr.mapping.holder.*;
+import org.mtr.mapping.tool.HolderBase;
+
+import java.util.List;
+
+import static com.lx862.jcm.mod.util.JCMUtil.playerHoldingBrush;
 
 public class LightLanternBlock extends DirectionalBlock {
+
+    public static final BooleanProperty LIT = BlockProperties.POWERED;
 
     public LightLanternBlock(BlockSettings settings) {
         super(settings);
@@ -13,5 +21,25 @@ public class LightLanternBlock extends DirectionalBlock {
     @Override
     public VoxelShape getOutlineShape2(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return VoxelUtil.getShape16(0, 0, 0, 16, 8, 16);
+    }
+
+    @Override
+    public ActionResult onUse2(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient()) {
+            ItemStack heldItem = player.getStackInHand(hand);
+
+            if (playerHoldingBrush(player)) {
+                BlockState newState = state.cycle(new Property<>(LIT.data));
+
+                world.setBlockState(pos, newState);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void addBlockProperties(List<HolderBase<?>> properties) {
+        super.addBlockProperties(properties);
+        properties.add(LIT);
     }
 }
