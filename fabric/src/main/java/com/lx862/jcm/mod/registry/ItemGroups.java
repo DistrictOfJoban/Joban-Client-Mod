500751ebb2e9d0e1f14e1614acbd01a4acccdb52
LX862 <kenny.mh.hui@outlook.com>
2024-06-18 17:05:58 +0800

    Restore PIDS Item Group

--- fabric/src/main/java/com/lx862/jcm/mod/registry/ItemGroups.java
+++ fabric/src/main/java/com/lx862/jcm/mod/registry/ItemGroups.java
@@ -1,14 +1,18 @@
 package com.lx862.jcm.mod.registry;
 
 import com.lx862.jcm.mod.Constants;
-import org.mtr.mapping.holder.Identifier;
 import org.mtr.mapping.holder.ItemConvertible;
 import org.mtr.mapping.holder.ItemStack;
 import org.mtr.mapping.registry.CreativeModeTabHolder;
 
 public class ItemGroups {
-    public static final CreativeModeTabHolder JCM_MAIN = JCMRegistry.REGISTRY.createCreativeModeTabHolder(
+    public static final CreativeModeTabHolder MAIN = JCMRegistry.REGISTRY.createCreativeModeTabHolder(
             Constants.id("main"),
             () -> new ItemStack(new ItemConvertible(Blocks.MTR_STAIRS.get().data))
+    );
+
+    public static final CreativeModeTabHolder PIDS = JCMRegistry.REGISTRY.createCreativeModeTabHolder(
+            Constants.id("pids"),
+            () -> new ItemStack(new ItemConvertible(Blocks.RV_PIDS.get().data))
     );
 }
