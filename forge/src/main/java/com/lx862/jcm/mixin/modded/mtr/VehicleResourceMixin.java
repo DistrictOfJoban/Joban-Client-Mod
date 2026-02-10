dab8a2f2cc0704f8f67cdd8121d642ba88bfbdd0
AmberFrost <kenny.mh.hui@outlook.com>
2026-02-10 18:28:39 +0800

    Fix build

--- forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/VehicleResourceMixin.java
+++ forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/VehicleResourceMixin.java
@@ -1,5 +1,6 @@
 package com.lx862.jcm.mixin.modded.mtr;
 
+import com.lx862.jcm.mod.resources.MTRContentResourceManager;
 import com.lx862.jcm.mod.scripting.mtr.MTRScripting;
 import com.lx862.jcm.mod.scripting.mtr.render.ScriptRenderManager;
 import com.lx862.jcm.mod.scripting.mtr.sound.ScriptSoundManager;
@@ -7,15 +8,14 @@ import com.lx862.jcm.mod.scripting.mtr.vehicle.VehicleScriptInstance;
 import com.lx862.mtrscripting.core.ScriptInstance;
 import com.lx862.mtrscripting.data.UniqueKey;
 import com.lx862.mtrscripting.util.ScriptVector3f;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.mtr.core.data.VehicleCar;
 import org.mtr.core.tool.Vector;
-import org.mtr.mapping.holder.Direction;
-import org.mtr.mapping.holder.MinecraftClient;
-import org.mtr.mapping.holder.World;
+import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
+import org.mtr.mapping.holder.*;
 import org.mtr.mod.data.VehicleExtension;
-import org.mtr.mod.render.MainRenderer;
-import org.mtr.mod.render.QueuedRenderLayer;
-import org.mtr.mod.render.StoredMatrixTransformations;
+import org.mtr.mod.render.*;
 import org.mtr.mod.resource.VehicleResource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,25 +25,44 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = VehicleResource.class, remap = false)
 public abstract class VehicleResourceMixin {
     @Inject(method = "queue(Lorg/mtr/mod/render/StoredMatrixTransformations;Lorg/mtr/mod/data/VehicleExtension;IIIZ)V", at = @At("HEAD"))
-    private void renderScript(StoredMatrixTransformations storedMatrixTransformations, VehicleExtension vehicle, int carNumber, int totalCars, int light, boolean noOpenDoorways, CallbackInfo ci) {
-        int carIndex = 0;
-        for(VehicleCar vehicleCar : vehicle.vehicleExtraData.immutableVehicleCars) {
-            ScriptInstance<?> scriptInstance = MTRScripting.getScriptManager().getInstanceManager().getInstance(new UniqueKey("mtr", "vehicle", vehicle.getId(), vehicleCar.getVehicleId()));
-            if(!(scriptInstance instanceof VehicleScriptInstance)) return;
-            VehicleScriptInstance vehicleScriptInstance = (VehicleScriptInstance)scriptInstance;
+    private void renderCarScript(StoredMatrixTransformations storedMatrixTransformations, VehicleExtension vehicle, int carNumber, int totalCars, int light, boolean noOpenDoorways, CallbackInfo ci) {
+        VehicleCar vehicleCar = vehicle.vehicleExtraData.immutableVehicleCars.get(carNumber);
+        String scriptGroupId = MTRContentResourceManager.getVehicleScriptGroupId(vehicleCar.getVehicleId());
+        if(scriptGroupId == null) return;
 
-            ScriptRenderManager scriptRenderManager = ((VehicleScriptInstance)scriptInstance).renderManagers[carIndex];
-            ScriptSoundManager scriptSoundManager = ((VehicleScriptInstance)scriptInstance).soundManagers[carIndex];
+        ScriptInstance<?> scriptInstance = MTRScripting.getScriptManager().getInstanceManager().getInstance(new UniqueKey("vehicle", vehicle.getHexId(), scriptGroupId));
+        if(!(scriptInstance instanceof VehicleScriptInstance)) return;
 
-            MainRenderer.scheduleRender(QueuedRenderLayer.TEXT, (graphicsHolder, vec) -> {
-                World world = World.cast(MinecraftClient.getInstance().getWorldMapped());
+        ScriptRenderManager carRenderManager = ((VehicleScriptInstance)scriptInstance).renderManagers[carNumber];
+        ScriptSoundManager carSoundManager = ((VehicleScriptInstance)scriptInstance).soundManagers[carNumber];
 
-                Vector carMidPos = vehicle.getHeadPosition(); // TODO: Get middle pos of each car...
-                ScriptVector3f basePos = new ScriptVector3f((float)carMidPos.x, (float)carMidPos.y, (float)carMidPos.z);
-                scriptRenderManager.invoke(world, storedMatrixTransformations.copy(), Direction.NORTH, light);
-                scriptSoundManager.invoke(world, basePos);
-            });
-            carIndex++;
+        StoredMatrixTransformations newTransform = storedMatrixTransformations.copy();
+        newTransform.add(gh -> gh.translate(0, -1, 0)); // Replicate behaviour from MTR 3. Not sure if we should do it here tho?
+
+        World world = World.cast(MinecraftClient.getInstance().getWorldMapped());
+
+        double x = 0, y = 0, z = 0;
+        int total = 0;
+        ObjectArrayList<ObjectObjectImmutablePair<Vector, Vector>> bogiePoses = vehicle.getVehicleCarsAndPositions().get(carNumber).right();
+        for(ObjectObjectImmutablePair<Vector, Vector> bogiePos : bogiePoses) {
+            x += bogiePos.left().x;
+            y += bogiePos.left().y;
+            z += bogiePos.left().z;
+            x += bogiePos.right().x;
+            y += bogiePos.right().y;
+            z += bogiePos.right().z;
+            total += 2;
+        }
+
+        Vector bogieMidPos = new Vector(x / total, y / total, z / total);
+        ScriptVector3f soundPos = new ScriptVector3f((float)bogieMidPos.x, (float)bogieMidPos.y, (float)bogieMidPos.z);
+
+        if(carRenderManager != null) {
+            carRenderManager.invoke(world, newTransform, Direction.NORTH, light);
+        }
+
+        if(carSoundManager != null) {
+            carSoundManager.invoke(world, soundPos);
         }
     }
 }
