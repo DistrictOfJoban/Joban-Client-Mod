afb9c98b3097a563beb4bbfbb363afb039bb0cd6
AmberFrost <kenny.mh.hui@outlook.com>
2026-03-23 17:48:50 +0800

    im fairly impressed this works at all

--- forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/ModelPropertiesPartMixin.java
+++ forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/ModelPropertiesPartMixin.java
@@ -6,19 +6,24 @@ import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectDoubleImmutablePair
 import org.mtr.mapping.holder.Box;
 import org.mtr.mapping.holder.Identifier;
 import org.mtr.mod.data.VehicleExtension;
+import org.mtr.mod.generated.resource.ModelPropertiesPartSchema;
 import org.mtr.mod.render.StoredMatrixTransformations;
-import org.mtr.mod.resource.ModelPropertiesPart;
+import org.mtr.mod.resource.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ModelPropertiesPart.class, remap = false)
-public class ModelPropertiesPartMixin {
+public class ModelPropertiesPartMixin extends ModelPropertiesPartSchema {
+    protected ModelPropertiesPartMixin(PartCondition condition, RenderStage renderStage, PartType type, double displayXPadding, double displayYPadding, String displayColorCjk, String displayColor, double displayMaxLineHeight, double displayCjkSizeRatio, double displayPadZeros, DisplayType displayType, String displayDefaultText, double doorXMultiplier, double doorZMultiplier, DoorAnimationType doorAnimationType, long renderFromOpeningDoorTime, long renderUntilOpeningDoorTime, long renderFromClosingDoorTime, long renderUntilClosingDoorTime, long flashOffTime, long flashOnTime) {
+        super(condition, renderStage, type, displayXPadding, displayYPadding, displayColorCjk, displayColor, displayMaxLineHeight, displayCjkSizeRatio, displayPadZeros, displayType, displayDefaultText, doorXMultiplier, doorZMultiplier, doorAnimationType, renderFromOpeningDoorTime, renderUntilOpeningDoorTime, renderFromClosingDoorTime, renderUntilClosingDoorTime, flashOffTime, flashOnTime);
+    }
+
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void jsblock$hideDisplayParts(Identifier texture, StoredMatrixTransformations storedMatrixTransformations, VehicleExtension vehicle, int carNumber, int[] scrollingDisplayIndexTracker, int light, ObjectArrayList<ObjectDoubleImmutablePair<Box>> openDoorways, boolean fromResourcePackCreator, CallbackInfo ci) {
         String vehicleId = vehicle.getVehicleCarsAndPositions().get(carNumber).left().getVehicleId();
-        if(MTRContentResourceManager.shouldHideDisplayParts(vehicleId)) {
+        if(type == PartType.DISPLAY && MTRContentResourceManager.shouldHideDisplayParts(vehicleId)) {
             ci.cancel();
         }
     }
