d045eff1f862798f7087f02c23cca4ff9b68ceb3
AmberFrost <kenny.mh.hui@outlook.com>
2026-04-05 02:56:38 +0800

    Fix build

--- forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/ObjModelAccessor.java
+++ forge/src/main/java/com/lx862/jcm/mixin/modded/mtr/ObjModelAccessor.java
@@ -4,11 +4,15 @@ import org.mtr.mapping.mapper.OptimizedModel;
 import org.mtr.mapping.render.model.RawMesh;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.List;
 
 @Mixin(value = OptimizedModel.ObjModel.class, remap = false)
 public interface ObjModelAccessor {
+    @Invoker("<init>")
+    OptimizedModel.ObjModel createNew(List<RawMesh> rawMeshes, boolean flipTextureV, float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
+
     @Accessor
     List<RawMesh> getRawMeshes();
 }
