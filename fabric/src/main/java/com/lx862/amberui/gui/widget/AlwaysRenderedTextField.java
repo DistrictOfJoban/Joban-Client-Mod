b06eff563bd03a2e65accd8dccb593d852c0ad41
AmberFrost <kenny.mh.hui@outlook.com>
2026-09-14 18:13:39 +0800

    Fix nullable annotation

--- fabric/src/main/java/com/lx862/amberui/gui/widget/AlwaysRenderedTextField.java
+++ fabric/src/main/java/com/lx862/amberui/gui/widget/AlwaysRenderedTextField.java
@@ -1,21 +1,20 @@
 package com.lx862.amberui.gui.widget;
 
-import org.jetbrains.annotations.Nullable;
 import org.mtr.mapping.holder.MutableText;
 import org.mtr.mapping.mapper.GraphicsHolder;
 import org.mtr.mapping.mapper.TextFieldWidgetExtension;
 import org.mtr.mapping.tool.TextCase;
 
 public class AlwaysRenderedTextField extends TextFieldWidgetExtension {
-    public AlwaysRenderedTextField(int x, int y, int width, int height, int maxLength, TextCase textCase, @Nullable String filter, @Nullable String suggestion) {
+    public AlwaysRenderedTextField(int x, int y, int width, int height, int maxLength, TextCase textCase, String filter, String suggestion) {
         super(x, y, width, height, maxLength, textCase, filter, suggestion);
     }
 
-    public AlwaysRenderedTextField(int x, int y, int width, int height, String message, int maxLength, TextCase textCase, @Nullable String filter, @Nullable String suggestion) {
+    public AlwaysRenderedTextField(int x, int y, int width, int height, String message, int maxLength, TextCase textCase, String filter, String suggestion) {
         super(x, y, width, height, message, maxLength, textCase, filter, suggestion);
     }
 
-    public AlwaysRenderedTextField(int x, int y, int width, int height, MutableText text, int maxLength, TextCase textCase, @Nullable String filter, @Nullable String suggestion) {
+    public AlwaysRenderedTextField(int x, int y, int width, int height, MutableText text, int maxLength, TextCase textCase, String filter, String suggestion) {
         super(x, y, width, height, text, maxLength, textCase, filter, suggestion);
     }
 
